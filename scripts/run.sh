9037adf2b8e18b89daecd274512a8b164eb8d70c
aon <21188659+aon@users.noreply.github.com>
2024-02-19 09:55:31 -0300

    Add logs to script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 # Source env variables if exist
+echo "Sourcing environment variables if exist"
 if [ -f ../.env ]; then
+    echo ".env file found. Sourcing it."
     source ../.env
 fi
 
